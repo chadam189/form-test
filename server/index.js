17aae3659c2c7abbf4fbaae7430a660700adce4f
Adam Miller <chadam189@gmail.com>
2018-02-16 13:11:57 -0800

    Create basic server using Express

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('/', (req, res) => {
+	res.sendStatus(200);
+});
+
+app.get('*', (req, res) => {
+	res.sendStatus(404);
+});
+
+
+app.listen(PORT, console.log(`Form Test Server is listening on ${PORT}!`));
